c96b82a932ccfce88fc97bb0b1bfb06bd15e7127
Steven Simon <ssimon@Stevens-MacBook-Pro.local>
2023-07-05 12:58:30 -0500

    Removed lazy loading attribute

--- js/modals.js
+++ js/modals.js
@@ -45,32 +45,32 @@ $(document).ready(function(){
          $('#modal-'+clicker).css('z-index', '1000');
          $('#modal-'+clicker).fadeIn('fast');
          $('#modal-'+clicker).scrollTop(0);
-         el = $(document).scrollTop();
          $('body').addClass('my-body-noscroll-class');
          pop.play();
-         window.history.pushState({}, '', '/'+clicker);
+         window.history.pushState({}, '', '/portfolio/#modal-'+clicker);
+         el = $(document).scrollTop();
          return false;
       });//See More
 
       $('#'+clicker+'-exit').click(function(){
          $('#modal-'+clicker).fadeOut('fast');
          $('body').removeClass('my-body-noscroll-class');
-         $(document).scrollTop(el);
          $('#video-'+clicker).attr('src', $('#video-'+clicker).attr('src'));
          $('#video-'+clicker).css('display', 'block');
          pop.play();
          window.history.go(-1);
+         $(document).scrollTop(el);
          return false;
       });//Close Top
 
       $('#'+clicker+'-exit-bottom').click(function(){
          $('#modal-'+clicker).fadeOut('fast');
          $('body').removeClass('my-body-noscroll-class');
-         $(document).scrollTop(el);
          $('#video-'+clicker).attr('src', $('#video-'+clicker).attr('src'));
          $('#video-'+clicker).css('display', 'block');
          pop.play();
          window.history.go(-1);
+         $(document).scrollTop(el);
          return false;
       });//Close Bottom
 
